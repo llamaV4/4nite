495004af4105719ac2d0d9ebceffa315e338b1e3
alex <976agg@gmail.com>
2026-04-07 02:40:51 -0400

    :)

---
+++ tictactoe.py
@@ -0,0 +1,9 @@
+board = [" ", " ", " ",
+         " ", " ", " ",
+         " ", " ", " "]
+def print_board():
+    print(board[0] + "|" + board[1] + "|" + board[2])
+    print("-+-+-")
+    print(board[3] + "|" + board[4] + "|" + board[5])
+    print("-+-+-")
+    print(board[6] + "|" + board[7] + "|" + board[8])
